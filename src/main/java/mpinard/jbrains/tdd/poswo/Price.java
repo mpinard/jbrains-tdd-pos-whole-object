ed8b47c225682596cb66b31e6dafecd2c3fae2ca
Michael Pinard <mike.pinard@adp.com>
2018-09-02 19:00:34 -0400

    Price now has value and unused value constructor.

--- src/main/java/mpinard/jbrains/tdd/poswo/Price.java
+++ src/main/java/mpinard/jbrains/tdd/poswo/Price.java
@@ -1,4 +1,16 @@
 package mpinard.jbrains.tdd.poswo;
 
+import java.math.BigDecimal;
+
 public final class Price {
+    
+    private final BigDecimal value;
+    
+    public Price() {
+        this(BigDecimal.ZERO);
+    }
+
+    public Price(final BigDecimal value) {
+        this.value = value;
+    }
 }
